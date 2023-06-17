5b2e10bb215fe8525428ce6a926768e912a3f41e
Jianfeng <jjfwang@outlook.com>
2023-06-17 16:12:28 +0800

    page adjustment

--- apps/frontend/src/components/about.tsx
+++ apps/frontend/src/components/about.tsx
@@ -1,12 +1,21 @@
+"use client";
+
 import * as React from 'react';
+import { Box, Text, Grid, Heading, VStack } from "@chakra-ui/react";
 
 export interface IAboutProps {
 }
 
 export function About (props: IAboutProps) {
   return (
-    <div>
-      
-    </div>
+    <Box textAlign="center" fontSize="sm">
+      <Grid p={3}>
+        <VStack spacing={8}>
+          <Text>
+            This is the about page.
+          </Text>
+        </VStack>
+      </Grid>
+    </Box>
   );
 }
--- apps/frontend/src/components/navigation.tsx
+++ apps/frontend/src/components/navigation.tsx
@@ -7,11 +7,12 @@ import {
   Button,
   ButtonGroup,
   Divider,
+  Image,
 } from "@chakra-ui/react";
 import { useSession, signIn, signOut } from "next-auth/react";
 import { useState } from "react";
 
-export interface INavigationProps {}
+export interface INavigationProps { }
 
 export default function Navigation(props: INavigationProps) {
   const { data: session } = useSession();
@@ -27,7 +28,9 @@ export default function Navigation(props: INavigationProps) {
         <ButtonGroup gap="4" p={5}>
           {session ? (
             <>
-              Signed in as {session.user?.email} <br />
+              <Image src={session.user?.image ?? ''} width={37}
+                height={37} alt='profile'></Image>
+              {session.user?.name} <br />
               <Button
                 colorScheme="blue"
                 variant="outline"
